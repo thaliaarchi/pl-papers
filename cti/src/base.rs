66eb2ddf757ac824310d300bbf3dc3cbb37a3e96
Thalia Archibald <thalia@archibald.dev>
2023-01-21 18:52:30 -0700

    cti/Rust: Use Result instead of panicking

--- cti/src/base.rs
+++ cti/src/base.rs
@@ -38,6 +38,14 @@ pub enum Val {
 pub type Env = Vec<Val>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Error {
+    Index { index: usize, len: usize },
+    Type,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Interpreter {
     fresh: Var,
     block: Vec<Exp>,
@@ -52,109 +60,115 @@ impl Interpreter {
     }
 
     // NBE-style polymorphic lift operator
-    pub fn lift(&mut self, v: Val) -> Exp {
+    pub fn lift(&mut self, v: Val) -> Result<Exp> {
         match v {
-            Cst(n) => Lit(n),
+            Cst(n) => Ok(Lit(n)),
             Tup(a, b) => match (*a, *b) {
-                (Code(u), Code(v)) => self.reflect(Cons(u, v)),
-                _ => panic!(),
+                (Code(u), Code(v)) => Ok(self.reflect(Cons(u, v))),
+                _ => Err(Error::Type),
             },
             Clo(mut env2, e2) => {
                 env2.push(Code(Box::new(self.fresh())));
                 env2.push(Code(Box::new(self.fresh())));
-                let e = Box::new(self.reifyc_evalms(&env2, *e2));
-                self.reflect(Lam(e))
+                let e = Box::new(self.reifyc_evalms(&env2, *e2)?);
+                Ok(self.reflect(Lam(e)))
             }
-            Code(e) => self.reflect(Lift(e)),
+            Code(e) => Ok(self.reflect(Lift(e))),
         }
     }
 
-    pub fn liftc(&mut self, v: Val) -> Val {
-        Code(Box::new(self.lift(v)))
+    pub fn liftc(&mut self, v: Val) -> Result<Val> {
+        Ok(Code(Box::new(self.lift(v)?)))
     }
 
     // Multi-stage evaluation
-    pub fn evalms(&mut self, env: &Env, e: Exp) -> Val {
+    pub fn evalms(&mut self, env: &Env, e: Exp) -> Result<Val> {
         match e {
-            Lit(n) => Cst(n),
-            Var(n) => env[n as usize].clone(),
-            Cons(e1, e2) => Tup(
-                Box::new(self.evalms(env, *e1)),
-                Box::new(self.evalms(env, *e2)),
-            ),
-            Lam(e) => Clo(env.clone(), e),
+            Lit(n) => Ok(Cst(n)),
+            Var(n) => match env.get(n as usize) {
+                Some(v) => Ok(v.clone()),
+                None => Err(Error::Index {
+                    index: n,
+                    len: env.len(),
+                }),
+            },
+            Cons(e1, e2) => Ok(Tup(
+                Box::new(self.evalms(env, *e1)?),
+                Box::new(self.evalms(env, *e2)?),
+            )),
+            Lam(e) => Ok(Clo(env.clone(), e)),
             Let(e1, e2) => {
                 let mut env1 = env.clone();
-                env1.push(self.evalms(env, *e1));
+                env1.push(self.evalms(env, *e1)?);
                 self.evalms(&env1, *e2)
             }
-            App(e1, e2) => match (self.evalms(env, *e1), self.evalms(env, *e2)) {
-                (Code(s1), Code(s2)) => self.reflectc(App(s1, s2)),
+            App(e1, e2) => match (self.evalms(env, *e1)?, self.evalms(env, *e2)?) {
+                (Code(s1), Code(s2)) => Ok(self.reflectc(App(s1, s2))),
                 (Clo(env1, s1), v2) => {
                     let mut env3 = env1.clone();
                     env3.push(Clo(env1, s1.clone()));
                     env3.push(v2);
                     self.evalms(&env3, *s1)
                 }
-                _ => panic!(),
+                _ => Err(Error::Type),
             },
-            If(c, a, b) => match self.evalms(env, *c) {
+            If(c, a, b) => match self.evalms(env, *c)? {
                 Code(c1) => {
-                    let a2 = Box::new(self.reifyc_evalms(&env, *a));
-                    let b2 = Box::new(self.reifyc_evalms(&env, *b));
-                    self.reflectc(If(c1, a2, b2))
+                    let a2 = Box::new(self.reifyc_evalms(&env, *a)?);
+                    let b2 = Box::new(self.reifyc_evalms(&env, *b)?);
+                    Ok(self.reflectc(If(c1, a2, b2)))
                 }
                 Cst(n) => self.evalms(env, *if n != 0 { a } else { b }),
-                _ => panic!(),
+                _ => Err(Error::Type),
             },
-            IsNum(e) => match self.evalms(env, *e) {
-                Code(s) => self.reflectc(IsNum(s)),
-                Cst(_) => Cst(1),
-                _ => Cst(0),
+            IsNum(e) => match self.evalms(env, *e)? {
+                Code(s) => Ok(self.reflectc(IsNum(s))),
+                Cst(_) => Ok(Cst(1)),
+                _ => Ok(Cst(0)),
             },
-            IsCons(e) => match self.evalms(env, *e) {
-                Code(s) => self.reflectc(IsCons(s)),
-                Tup(_, _) => Cst(1),
-                _ => Cst(0),
+            IsCons(e) => match self.evalms(env, *e)? {
+                Code(s) => Ok(self.reflectc(IsCons(s))),
+                Tup(_, _) => Ok(Cst(1)),
+                _ => Ok(Cst(0)),
             },
-            Car(e) => match self.evalms(env, *e) {
-                Code(s) => self.reflectc(Car(s)),
-                Tup(a, _) => *a,
-                _ => panic!(),
+            Car(e) => match self.evalms(env, *e)? {
+                Code(s) => Ok(self.reflectc(Car(s))),
+                Tup(a, _) => Ok(*a),
+                _ => Err(Error::Type),
             },
-            Cdr(e) => match self.evalms(env, *e) {
-                Code(s) => self.reflectc(Cdr(s)),
-                Tup(_, b) => *b,
-                _ => panic!(),
+            Cdr(e) => match self.evalms(env, *e)? {
+                Code(s) => Ok(self.reflectc(Cdr(s))),
+                Tup(_, b) => Ok(*b),
+                _ => Err(Error::Type),
             },
-            Plus(e1, e2) => match (self.evalms(env, *e1), self.evalms(env, *e2)) {
-                (Code(s1), Code(s2)) => self.reflectc(Plus(s1, s2)),
-                (Cst(n1), Cst(n2)) => Cst(n1 + n2),
-                _ => panic!(),
+            Plus(e1, e2) => match (self.evalms(env, *e1)?, self.evalms(env, *e2)?) {
+                (Code(s1), Code(s2)) => Ok(self.reflectc(Plus(s1, s2))),
+                (Cst(n1), Cst(n2)) => Ok(Cst(n1 + n2)),
+                _ => Err(Error::Type),
             },
-            Minus(e1, e2) => match (self.evalms(env, *e1), self.evalms(env, *e2)) {
-                (Code(s1), Code(s2)) => self.reflectc(Minus(s1, s2)),
-                (Cst(n1), Cst(n2)) => Cst(n1 - n2),
-                _ => panic!(),
+            Minus(e1, e2) => match (self.evalms(env, *e1)?, self.evalms(env, *e2)?) {
+                (Code(s1), Code(s2)) => Ok(self.reflectc(Minus(s1, s2))),
+                (Cst(n1), Cst(n2)) => Ok(Cst(n1 - n2)),
+                _ => Err(Error::Type),
             },
-            Times(e1, e2) => match (self.evalms(env, *e1), self.evalms(env, *e2)) {
-                (Code(s1), Code(s2)) => self.reflectc(Times(s1, s2)),
-                (Cst(n1), Cst(n2)) => Cst(n1 * n2),
-                _ => panic!(),
+            Times(e1, e2) => match (self.evalms(env, *e1)?, self.evalms(env, *e2)?) {
+                (Code(s1), Code(s2)) => Ok(self.reflectc(Times(s1, s2))),
+                (Cst(n1), Cst(n2)) => Ok(Cst(n1 * n2)),
+                _ => Err(Error::Type),
             },
-            Eq(e1, e2) => match (self.evalms(env, *e1), self.evalms(env, *e2)) {
-                (Code(s1), Code(s2)) => self.reflectc(Eq(s1, s2)),
-                (Cst(n1), Cst(n2)) => Cst(if n1 == n2 { 1 } else { 0 }),
-                _ => panic!(),
+            Eq(e1, e2) => match (self.evalms(env, *e1)?, self.evalms(env, *e2)?) {
+                (Code(s1), Code(s2)) => Ok(self.reflectc(Eq(s1, s2))),
+                (Cst(n1), Cst(n2)) => Ok(Cst(if n1 == n2 { 1 } else { 0 })),
+                _ => Err(Error::Type),
             },
             Lift(e) => {
-                let v = self.evalms(env, *e);
+                let v = self.evalms(env, *e)?;
                 self.liftc(v)
             }
-            Run(b, e) => match self.evalms(env, *b) {
+            Run(b, e) => match self.evalms(env, *b)? {
                 Code(b1) => {
-                    let e = Box::new(self.reifyc_evalms(env, *e));
-                    self.reflectc(Run(b1, e))
+                    let e = Box::new(self.reifyc_evalms(env, *e)?);
+                    Ok(self.reflectc(Run(b1, e)))
                 }
                 _ => {
                     let e = {
@@ -162,9 +176,9 @@ impl Interpreter {
                         self.fresh = env.len();
                         let mut block = Vec::new();
                         mem::swap(&mut block, &mut self.block);
-                        let e = match self.evalms(env, *e) {
+                        let e = match self.evalms(env, *e)? {
                             Code(e) => *e,
-                            _ => panic!(),
+                            _ => return Err(Error::Type),
                         };
                         self.fresh = fresh;
                         self.block = block;
@@ -176,18 +190,17 @@ impl Interpreter {
         }
     }
 
-    fn evalmsg(&mut self, env: &Env, e: Exp) -> Val {
+    fn evalmsg(&mut self, env: &Env, e: Exp) -> Result<Val> {
         let fresh = self.fresh;
         let mut block = Vec::new();
         mem::swap(&mut block, &mut self.block);
-        let res = self.evalms(env, e);
-        let v = match res {
-            Code(last) => Code(Box::new(self.fold_let(*last))),
-            _ => panic!(),
-        };
+        let res = self.evalms(env, e)?;
         self.fresh = fresh;
         self.block = block;
-        v
+        match res {
+            Code(last) => Ok(Code(Box::new(self.fold_let(*last)))),
+            _ => Err(Error::Type),
+        }
     }
 
     fn fresh(&mut self) -> Exp {
@@ -204,17 +217,17 @@ impl Interpreter {
         Code(Box::new(self.reflect(s)))
     }
 
-    fn reifyc_evalms(&mut self, env: &Env, e: Exp) -> Exp {
+    fn reifyc_evalms(&mut self, env: &Env, e: Exp) -> Result<Exp> {
         let fresh = self.fresh;
         let mut block = Vec::new();
         mem::swap(&mut block, &mut self.block);
-        let v = self.evalms(env, e);
+        let v = self.evalms(env, e)?;
         self.fresh = fresh;
         self.block = block;
-        self.fold_let(match v {
+        Ok(self.fold_let(match v {
             Code(e) => *e,
-            _ => panic!(),
-        })
+            _ => return Err(Error::Type),
+        }))
     }
 
     fn fold_let(&self, last: Exp) -> Exp {
