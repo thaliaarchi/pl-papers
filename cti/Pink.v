a6ba80de069a86238695bb0f60b69179233db183
Thalia Archibald <thalia@archibald.dev>
2023-01-23 03:48:57 -0700

    cti/Coq: Add Pink tests

--- cti/Pink.v
+++ cti/Pink.v
@@ -1,4 +1,5 @@
-Require Import String.
+Require Import String List.
+Import ListNotations.
 From CTI Require Import BaseFix.
 Open Scope string_scope.
 
@@ -40,3 +41,9 @@ Definition pink_eval :=
     (EIf (EEq "cons"   (ECar exp)) (maybe_lift (ECons (EApp (eval (ECadr exp)) env) (EApp (eval (ECaddr exp)) env)))
     (EIf (EEq "app"    (ECar exp)) (EApp (EApp (eval (ECadr exp)) env) (EApp (eval (ECaddr exp)) env))
     (EErr ErrPinkEval))))))))))))))))))))).
+
+Module Tests.
+Example ex0 : ((0, []), VNat 5)                  = eval0 (EApp (EApp (EApp pink_eval (ELam (EVar 1))) (ENat 5)) (ELam (EErr ErrUnboundVar))). reflexivity. Qed.
+Example ex1 : ((0, []), VPair (VNat 5) (VNat 6)) = eval0 (EApp (EApp (EApp pink_eval (ELam (EVar 1))) (ECons "cons" (ECons (ENat 5) (ECons (ENat 6) ".")))) (ELam (EErr ErrUnboundVar))). reflexivity. Qed.
+Example ex2 : ((0, []), VNat 3)                  = eval0 (EApp (EApp (EApp pink_eval (ELam (EVar 1))) (ECons "app" (ECons (ECons "lambda" (ECons "_" (ECons "x" (ECons (ECons "+" (ECons "x" (ECons (ENat 1) "."))) ".")))) (ECons (ENat 2) ".")))) (ELam (EErr ErrUnboundVar))). reflexivity. Qed.
+End Tests.
